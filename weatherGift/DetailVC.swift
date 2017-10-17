ad3aed929b644a380fe32e8968451ed7964c0c66
Stephen Casazza <stephencasazza@raymonds-mbp.bc.edu>
2017-10-17 14:50:03 -0400

    Chapter 7.1-7.7

---
+++ weatherGift/DetailVC.swift
@@ -0,0 +1,28 @@
+//
+//  DetailVC.swift
+//  weatherGift
+//
+//  Created by Stephen Casazza on 10/15/17.
+//  Copyright © 2017 Casazza. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationLabel.text = locationsArray[currentPage]
+
+    }
+
+}
